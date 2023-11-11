f19aa2d5b59a570062a3e578e11b653ff2159b67
abdullah <en>
2023-11-11 20:12:46 +0300

    task6_1_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import { Form } from 'react-bootstrap';
+import { calculateNewValue } from '@testing-library/user-event/dist/utils';
 function App() {
+  const [result, setResult] = useState("");
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name))
+  };
+  const clear = () => {
+    setResult("");
+  }
+  const backspace = () => {
+    setResult(result.slice(0, result.length - 1));
+  }
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (err) {
+      setResult("Error");
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div className="container">
+      <div class="calc">
+        <input type="text" value={result} className='result' />
+
+
+        <button name="CA" className="orang_color" onClick={clear} id="clear" >CA</button>
+        <button name="C" className="orang_color" onClick={backspace} id="backspace" >DEL</button>
+        <button name="/" className="orang_color" onClick={handleClick} >/</button>
+        <button name="*" className="orang_color" onClick={handleClick} >X</button>
+
+        <button name="7" onClick={handleClick} >7</button>
+        <button name="8" onClick={handleClick} >8</button>
+        <button name="9" onClick={handleClick} >9</button>
+
+        <button name="-" className="orang_color" onClick={handleClick} >-</button>
+        <button name="4" onClick={handleClick} >4</button>
+        <button name="5" onClick={handleClick} >5</button>
+        <button name="6" onClick={handleClick} >6</button>
+
+        <button name="+" className="orang_color" onClick={handleClick} >+</button>
+        <button name="1" onClick={handleClick} >1</button>
+        <button name="2" onClick={handleClick} >2</button>
+        <button name="3" onClick={handleClick} >3</button>
+
+        <button name="=" className="orang_color" onClick={calculate} >=</button>
+
+        <button name="0" className="bottom_num" onClick={handleClick} >0</button>
+        <button name="." className="bottom_num" onClick={handleClick} >.</button>
+
+      </div>
+
+
+
     </div>
+
+
+
   );
 }
+
 
 export default App;
